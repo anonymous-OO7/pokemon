772e0cea1f9b32a20998f3dab50a9be6f37d3e69
Gaurav kumar yadav <gaurav@Gauravs-MacBook-Air.local>
2021-09-15 17:52:40 +0530

    Updates

---
+++ precache-manifest.84d0c2360882c45866f0552185c6cf15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9df13a3ae4907e89f80566587dc3cb9",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "5861e5d6a1038085160b",
+    "url": "/pokemon/static/css/main.47f666f0.chunk.css"
+  },
+  {
+    "revision": "87c3aeaa0f6960155e16",
+    "url": "/pokemon/static/js/2.d09f8bdb.chunk.js"
+  },
+  {
+    "revision": "5861e5d6a1038085160b",
+    "url": "/pokemon/static/js/main.d6267598.chunk.js"
+  },
+  {
+    "revision": "c7b34e90fd5a4298f876",
+    "url": "/pokemon/static/js/runtime~main.9f7d124e.js"
+  }
+]);
